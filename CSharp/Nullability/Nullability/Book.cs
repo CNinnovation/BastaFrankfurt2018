0e0cb6034553173cf563a249039827064335f2b2
Christian Nagel <christian@christiannagel.com>
2018-02-20 08:47:38 +0100

    nullability

---
+++ CSharp/Nullability/Nullability/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Nullability
+{
+    public class Book
+    {
+        public Book(string title, string? publisher = null, string? isbn = null)
+        {
+            Title = title;
+            Publisher = publisher;
+            Isbn = isbn;
+        }
+        public string Title { get; set; }
+        public string? Publisher { get; set; }
+        public string? Isbn { get; set; }
+    }
+}
---
+++ CSharp/Nullability/Nullability/Program.cs
@@ -0,0 +1,35 @@
+﻿using LegacyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullability
+{
+    public class ThisIsNew
+    {
+        public void Foo(string s)
+        {
+            Console.WriteLine(s.ToUpper());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var legacy = new Legacy();
+            legacy.Foo(null); // passing null is ok
+
+            var newer = new ThisIsNew();
+            newer.Foo(null!); // passing null is not ok
+
+            Book[] books = new Book[3];
+            books[0] = new Book("Professional C# 7");
+            Console.WriteLine(books[0].Title);
+            string publisher = books[0].Publisher ?? string.Empty;
+
+            Console.WriteLine(books[0].Publisher!);
+        }
+    }
+}
